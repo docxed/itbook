508b1d64a74555b380419d70d31c40ac563f3b46
Akira <akira.ajeyb@gmail.com>
2026-03-03 17:30:35 +0700

    dev up

--- itbook/Program.cs
+++ itbook/Program.cs
@@ -102,7 +102,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseSerilogRequestLogging();
 
-// Authentication
+// Authentication 1
 app.UseAuthentication();
 app.UseAuthorization();
 
